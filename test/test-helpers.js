e9825921d3152e07eb2c9e81b8556872fa776a11
Steven Bull <stevenwbulljr@gmail.com>
2019-10-10 18:45:50 +0000

    update test with bcrypt passwords

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -233,24 +233,42 @@ function cleanTables(db) {
   );
 }
 
+function seedUsers(db, users) {
+  const preppedUsers = users.map( user => ({
+    ...user,
+    password: bcrypt.hashSync(user.password, 1)
+  }));
+
+  return db.into('thingful_users').insert(preppedUsers)
+    .then( () => {
+      db.raw(
+        'SELECT setval(\'thingful_users_id_seq\', ?)',
+        [users[users.length - 1].id]);
+    });
+}
+
 function seedThingsTables(db, users, things, reviews=[]) {
-  return db
-    .into('thingful_users')
-    .insert(users)
-    .then(() =>
-      db
-        .into('thingful_things')
-        .insert(things)
-    )
-    .then(() =>
-      reviews.length && db.into('thingful_reviews').insert(reviews)
+  return db.transaction( async trx => {
+    await seedUsers(trx, users);
+    await trx.into('thingful_things').insert(things);
+
+    await trx.raw(
+      'SELECT setval(\'blogful_things_id_seq\', ?)',
+      [things[things.length - 1].id]
     );
+
+    if (reviews.length) {
+      await trx.into('thingful_reviews').insert(reviews);
+      await trx.raw(
+        'SELECT setval(\'blogful_comments_id_seq\', ?)',
+        [reviews[reviews.length - 1].id],
+      );
+    }
+  });
 }
 
 function seedMaliciousThing(db, user, thing) {
-  return db
-    .into('thingful_users')
-    .insert([user])
+  return seedUsers(db, [user])
     .then(() =>
       db
         .into('thingful_things')
@@ -275,5 +293,6 @@ module.exports = {
   cleanTables,
   seedThingsTables,
   seedMaliciousThing,
-  makeAuthHeader
+  makeAuthHeader,
+  seedUsers
 };
