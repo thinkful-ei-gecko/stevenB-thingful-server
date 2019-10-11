82b3798294030481a6513f4409b2f1cb75202d9a
Steven Bull <stevenwbulljr@gmail.com>
2019-10-11 16:40:57 +0000

    add jwt authentication

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -1,5 +1,6 @@
 /* eslint-disable quotes */
 const knex = require('knex');
+const jwt = require('jsonwebtoken');
 const app = require('../src/app');
 const helpers = require('./test-helpers');
 
@@ -60,11 +61,32 @@ describe.only('Auth Endpoints', function() {
     });
 
     it(`responds 400 'invalid user_name or password' when bad password`, () => {
-      const userInvalidPass = { user_name: testUser.user_name, password: 'incorrect' }
+      const userInvalidPass = { user_name: testUser.user_name, password: 'incorrect' };
       return supertest(app)
         .post('/api/auth/login')
         .send(userInvalidPass)
-        .expect(400, { error: `Incorrect user_name or password` })
+        .expect(400, { error: `Incorrect user_name or password` });
+    });
+
+    it(`responds 200 and JWT auth token using secret when valid credentials`, () => {
+      const userValidCreds = {
+        user_name: testUser.user_name,
+        password: testUser.password,
+      };
+      const expectedToken = jwt.sign(
+        { user_id: testUser.id },
+        process.env.JWT_SECRET,
+        {
+          subject: testUser.user_name,
+          algorithm: 'HS256',
+        }
+      );
+      return supertest(app)
+        .post('/api/auth/login')
+        .send(userValidCreds)
+        .expect(200, {
+          authToken: expectedToken,
+        });
     });
   });
 });
