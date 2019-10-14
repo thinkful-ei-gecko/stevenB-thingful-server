955d41245f6cabfb7e1137428b8b5a6df44e2eaf
Steven Bull <stevenwbulljr@gmail.com>
2019-10-14 14:16:44 +0000

    finish registration authentication

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -29,19 +29,22 @@ usersRouter
           return res.status(400).json({ error: 'Username already taken' });
         }
 
-        const newUser = {
-          user_name,
-          password,
-          full_name,
-          nickname,
-          date_created: 'now()'
-        };
+        return UsersService.hashPassword(password)
+          .then( hashedPassword => {
+            const newUser = {
+              user_name,
+              password: hashedPassword,
+              full_name,
+              nickname,
+              date_created: 'now()'
+            };
 
-        return UsersService.insertUser(req.app.get('db'), newUser) 
-          .then( user => {
-            res.status(201)
-              .location(path.posix.join(req.originalUrl, `/${user.id}`))
-              .json(UsersService.serializeUser(user))
+            return UsersService.insertUser(req.app.get('db'), newUser) 
+              .then( user => {
+                res.status(201)
+                  .location(path.posix.join(req.originalUrl, `/${user.id}`))
+                  .json(UsersService.serializeUser(user));
+              });
           });
       })
       .catch(next);
